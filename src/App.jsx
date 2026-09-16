d635e4aea98e0e2cf6f9e673066aeecb7fd66bcb
Anmol Kumar Singh <singhanmol1866web@gmail.com>
2026-09-16 12:28:03 +0530

    fix router and basename

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,52 @@
+// import React from 'react'
+// import Home from './pages/home/Home'
+// import {BrowserRouter, Route, Routes} from "react-router-dom" 
+// import Nav from './components/nav/Nav'
+// import Footer from './components/Footer/Footer'
+// import Shop from './pages/shop/Shop'
+// import Cart from './pages/cart/Cart'
+// import Contact from './pages/contact/Contact'
+// function App() {
+//   return (
+//     <>
+//     <BrowserRouter basename="/E-commerce-">
+//     <Nav/>
+//     <Routes>
+//       <Route path='/' element={<Home/>}/>
+//       <Route path='/shop' element={<Shop/>}/>
+//       <Route path='/cart' element={<Cart/>}/>
+//       <Route path='/contact' element={<Contact/>}/>
+//     </Routes>
+//     <Footer/>
+//     </BrowserRouter>
+     
+//     </>
+//   )
+// }
+
+// export default App
+
 import React from 'react'
 import Home from './pages/home/Home'
-import {BrowserRouter, Route, Routes} from "react-router-dom" 
+import { HashRouter as Router, Route, Routes } from "react-router-dom"
 import Nav from './components/nav/Nav'
 import Footer from './components/Footer/Footer'
 import Shop from './pages/shop/Shop'
 import Cart from './pages/cart/Cart'
 import Contact from './pages/contact/Contact'
+
 function App() {
   return (
-    <>
-    <BrowserRouter basename="/E-commerce-project">
-    <Nav/>
-    <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/shop' element={<Shop/>}/>
-      <Route path='/cart' element={<Cart/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-    </Routes>
-    <Footer/>
-    </BrowserRouter>
-     
-    </>
+    <Router>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/shop' element={<Shop/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+      </Routes>
+      <Footer/>
+    </Router>
   )
 }
 
