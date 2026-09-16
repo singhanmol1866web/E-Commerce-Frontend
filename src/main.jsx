6193c3ccb49e78fb101e0da2c65d8f863286feb3
Anmol Kumar Singh <singhanmol1866web@gmail.com>
2026-09-16 12:20:11 +0530

    fix router and base path

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import { store } from './redux/store.js'
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import './index.css'
+// import App from './App.jsx'
+// import { Provider } from 'react-redux'
+// import { store } from './redux/store.js'
 
-createRoot(document.getElementById('root')).render(
- <Provider store={store}>
-    <App />
-    </Provider>
+// createRoot(document.getElementById('root')).render(
+//  <Provider store={store}>
+//     <App />
+//     </Provider>
+// )
+
+// main.jsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom' // BrowserRouter ki jagah HashRouter use karein
+import App from './App'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 )
